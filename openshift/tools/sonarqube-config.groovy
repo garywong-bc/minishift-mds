b2b210da4196f41b482cf12b400a7dac87752e3f
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2018-07-27 20:12:20 -0700

    Try 2

--- openshift/tools/sonarqube-config.groovy
+++ openshift/tools/sonarqube-config.groovy
@@ -3,7 +3,7 @@ app {
     name = 'sonarqube'
     namespaces { //can't call environments :(
         'build'{
-            namespace = 'empr-mds-tools'
+            namespace = 'minishift-mds-tools'
             disposable = true
         }
         'dev' {
